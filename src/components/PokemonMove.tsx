6d111be42f5d4e2a4e77b9dc0c47fcf101c54224
Caio Araruna Cavalcanti <caio.cavalcanti@gsoftware.com.br>
2024-09-24 16:56:26 -0300

    ajustes no front

--- src/components/PokemonMove.tsx
+++ src/components/PokemonMove.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../src/PokemonMove.css";
-import { IPokemonData } from "../types/pokemon";
 
 interface IMove {
   attackName: string;
@@ -8,23 +7,65 @@ interface IMove {
   power: number;
 }
 
-/* interface PokemonMoveProps {
-  number: number;
-  name: string;
-  moves: IMove[];
-} */
+const getImageForType = (moveType: string) => {
+  switch (moveType.toLowerCase()) {
+    case "normal":
+      return "/normalType.png";
+    case "grass":
+      return "/grassType.png";
+    case "fire":
+      return "/fireType.png";
+    case "dragon":
+      return "/dragonType.png";
+    case "steel":
+      return "/steelType.png";
+    case "dark":
+      return "/darkType.png";
+    case "ghost":
+      return "/ghostType.png";
+    case "flying":
+      return "/flyingType.png";
+    case "water":
+      return "/waterType.png";
+    case "bug":
+      return "/bugType.png";
+    case "psychic":
+      return "/psychicType.png";
+    case "poison":
+      return "/poisonType.png";
+    case "electric":
+      return "/electricType.png";
+    case "ground":
+      return "/groundType.png";
+    case "fighting":
+      return "/fightingType.png";
+    case "fairy":
+      return "/fairyType.png";
+    case "ice":
+      return "/iceType.png";
+    case "rock":
+      return "/rockType.png";
+    default:
+      return "/normalType.png"; // Default case, caso o tipo não seja reconhecido
+  }
+};
 
 const PokemonMove: React.FC<any> = (props) => {
   return (
     <div className="PokemonMove">
       <img className="PokemonImg" src={props.sprites} alt={props.name} />
-      <h2>
+      <h2 style={{ margin: "auto" }}>
         {props.name} (#{props.number})
       </h2>
       <ul>
         {props.moves.map((move: any, index: any) => (
-          <li key={index}>
-            {move.name} - {move.moveType} - Power: {move.power}
+          <li key={index} className="pokeInfoLine">
+            <img
+              src={getImageForType(move.moveType)}
+              alt={move.moveType}
+              className="MoveTypeImg"
+            />
+            {move.attackName} - {move.moveType} - Power: {move.power}
           </li>
         ))}
       </ul>
